c64fead1068b2706b6a7ff4459214367bc9bd55e
Murillo <murilloginez@outlook.com>
2020-09-15 17:07:27 -0300

    Criação das Classes e inserção do Database

--- company_blog/__init__.py
+++ company_blog/__init__.py
@@ -1,7 +1,25 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 
+####################################
+######## DATABASE SET UP ###########
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+####################################
+######## LOGIN CONFIGS ###########
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+####################################
 from company_blog.core.views import core
 from company_blog.error_pages.handlers import error_pages
 
